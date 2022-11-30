59684f073155c28c79caae75aba127eecc2b0f1d
DamianKonce <damian.koncewicz@aps.clinic>
2022-11-30 09:51:29 +0100

    testing current weather

--- src/components/__tests__/WeatherCurrent.test.tsx
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -1,11 +1,18 @@
 import WeatherCurrent from "../weatherCurrent/WeatherCurrent";
-import { fireEvent, render, waitFor } from "@testing-library/react-native";
+import {
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react-native";
 import { useNavigation } from "@react-navigation/native";
+import LocationService from "../../services/location/LocationService";
+import { act } from "react-test-renderer";
 
 jest.mock("@react-navigation/native", () => {
   return {
     ...jest.requireActual<object>("@react-navigation/native"),
-    useNavigation: jest.fn(),
+    useNavigation: jest.fn().mockReturnValue({ navigate: jest.fn() }),
   };
 });
 
@@ -13,6 +20,11 @@ describe("WeatherCurrent", () => {
   test("Should render correctly", () => {
     const wrapper = render(<WeatherCurrent />);
     wrapper.getByTestId("weather-current");
+  });
+
+  test("Should render label", () => {
+    const wrapper = render(<WeatherCurrent />);
+    wrapper.getByText("Weather at my position");
   });
 
   test("Should navigate to weather screen with location", async () => {
@@ -30,6 +42,54 @@ describe("WeatherCurrent", () => {
         latitude: 0,
         longitude: 0,
       });
+    });
+  });
+  describe("Loader", () => {
+    test("Should be rendered when position is being fetched", async () => {
+      let mockResolve!: (position: {
+        latitude: number;
+        longitude: number;
+      }) => void;
+
+      jest.spyOn(LocationService, "getCurrentPosition").mockImplementationOnce(
+        () =>
+          new Promise((resolve) => {
+            mockResolve = resolve;
+          })
+      );
+      const wrapper = render(<WeatherCurrent />);
+      const button = wrapper.getByTestId("weather-current");
+      fireEvent.press(button);
+
+      await expect(
+        wrapper.findByTestId("button-loading")
+      ).resolves.toBeDefined();
+
+      await act(async () => {
+        await mockResolve({ latitude: 0, longitude: 0 });
+      });
+    });
+
+    test("Should not be rendered when position has been fetched", () => {
+      const wrapper = render(<WeatherCurrent />);
+      const button = wrapper.getByTestId("weather-current");
+      fireEvent.press(button);
+
+      return waitForElementToBeRemoved(() =>
+        wrapper.getByTestId("button-loading")
+      );
+    });
+
+    test("Should not be rendered when fetching position has failed", async () => {
+      jest
+        .spyOn(LocationService, "getCurrentPosition")
+        .mockRejectedValueOnce(new Error(""));
+
+      const wrapper = render(<WeatherCurrent />);
+      const button = wrapper.getByTestId("weather-current");
+      fireEvent.press(button);
+
+      return expect(wrapper.findByTestId("button-loading")).rejects.toThrow();
     });
   });
 });
--- src/components/weatherCurrent/WeatherCurrent.tsx
+++ src/components/weatherCurrent/WeatherCurrent.tsx
@@ -1,18 +1,28 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "../button";
 import LocationService from "../../services/location/LocationService";
 
 const WeatherCurrent = () => {
+  const [loading, setLoading] = useState(false);
   const navitagion = useNavigation();
 
   const handleFetchWeather = useCallback(async () => {
-    const position = await LocationService.getCurrentPosition();
-    navitagion.navigate("Weather", position);
+    setLoading(true);
+    try {
+      const position = await LocationService.getCurrentPosition();
+      navitagion.navigate("Weather", position);
+    } catch (e) {}
+    setLoading(false);
   }, [navitagion]);
 
   return (
-    <Button label="" onPress={handleFetchWeather} testID="weather-current" />
+    <Button
+      label="Weather at my position"
+      onPress={handleFetchWeather}
+      testID="weather-current"
+      loading={loading}
+    />
   );
 };
 
